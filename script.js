d60b39024e895a57e5415a9a3cd4ee6cb5ec6384
Christal D <179974249+ccdita@users.noreply.github.com>
2025-05-31 14:06:33 -0400

    Add game round logic via playRound() to script.js
    This commit adds the playRound() function to script.js, initiating a
    single round of the game and returning the winner at the end.

--- script.js
+++ script.js
@@ -29,6 +29,58 @@ function getHumanChoice() {
     return humanChoice;
 }
 
-console.log(getComputerChoice());
-console.log(getHumanChoice());
+function playRound(humanChoice, computerChoice) {
+    let winner; 
+    // Switch block to determine the winner of the round
+    switch (humanChoice) {
+        case "rock":
+            switch (computerChoice) {
+                case "rock":
+                    winner = "draw";
+                    break;
+                case "paper":
+                    winner = "computer";
+                    break;
+                case "scissors":
+                    winner = "human";
+                    break;
+            }
+            break;
+        case "paper":
+            switch (computerChoice) {
+                case "rock":
+                    winner = "human";
+                    break;
+                case "paper":
+                    winner = "draw";
+                    break;
+                case "scissors":
+                    winner = "computer";
+                    break;
+            }
+            break;
+        case "scissors":
+            switch (computerChoice) {
+                case "rock":
+                    winner = "computer";
+                    break;
+                case "paper":
+                    winner = "human";
+                    break;
+                case "scissors":
+                    winner = "draw";
+                    break;
+            }
+            break;
+    }
 
+    if (winner == "human") {
+        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
+    } else if (winner == "computer") {
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`);
+    } else {
+        console.log(`Tie! You both chose ${humanChoice}.`);
+    }
+}
+
+playRound(getHumanChoice(), getComputerChoice());
